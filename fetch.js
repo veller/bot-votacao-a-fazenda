83019a92871f0fa6466e5fcacb556695b123b060
Veller <vellerbauer@gmail.com>
2020-11-19 19:46:58 -0300

    blah

--- fetch.js
+++ fetch.js
@@ -3,65 +3,41 @@ const http = require('http')
 
 let counter = 0
 
-// function vote(){
-//     const options = {
-//         method: 'POST',
-//         url: 'https://voting-vote-producer.r7.com/vote',
-//         headers: {
-//           authority: 'voting-vote-producer.r7.com',
-//           'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36',
-//           'content-type': 'application/x-www-form-urlencoded',
-//           accept: '*/*',
-//           origin: 'https://afazenda.r7.com',
-//           'sec-fetch-site': 'same-site',
-//           'sec-fetch-mode': 'cors',
-//           'sec-fetch-dest': 'empty',
-//           referer: 'https://afazenda.r7.com/a-fazenda-12/votacao',
-//           'accept-language': 'en-US,en;q=0.9,pt;q=0.8'
-//         },
-//         form: {voting_id: '268', alternative_id: '648'},
-//       };
+function vote(){
+    const options = {
+        method: 'POST',
+        url: 'https://voting-vote-producer.r7.com/vote',
+        headers: {
+          authority: 'voting-vote-producer.r7.com',
+          'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36',
+          'content-type': 'application/x-www-form-urlencoded',
+          accept: '*/*',
+          origin: 'https://afazenda.r7.com',
+          'sec-fetch-site': 'same-site',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-dest': 'empty',
+          referer: 'https://afazenda.r7.com/a-fazenda-12/votacao',
+          'accept-language': 'en-US,en;q=0.9,pt;q=0.8'
+        },
+        form: {voting_id: '268', alternative_id: '648'},
+      };
       
-//       request(options, function (error, response, body) {
-//         if (error) throw new Error(error);
+      request(options, function (error, response, body) {
+        if (error) throw new Error(error);
       
-//         console.log(response.statusCode);
-//         console.log(`votos: `, counter)
+        console.log(response.statusCode);
+        console.log(`votos: `, counter)
 
-//         if (response.statusCode === 200) counter++
-//       })
-// }
-
-const requestListener = function vote(req, res) {
-  const options = {
-    method: 'POST',
-    url: 'https://voting-vote-producer.r7.com/vote',
-    headers: {
-      authority: 'voting-vote-producer.r7.com',
-      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36',
-      'content-type': 'application/x-www-form-urlencoded',
-      accept: '*/*',
-      origin: 'https://afazenda.r7.com',
-      'sec-fetch-site': 'same-site',
-      'sec-fetch-mode': 'cors',
-      'sec-fetch-dest': 'empty',
-      referer: 'https://afazenda.r7.com/a-fazenda-12/votacao',
-      'accept-language': 'en-US,en;q=0.9,pt;q=0.8'
-    },
-    form: {voting_id: '268', alternative_id: '648'},
-  };
-  
-  request(options, function (error, response, body) {
-    if (error) throw new Error(error);
-  
-    console.log(response.statusCode);
-    console.log(`votos: `, counter)
-
-    if (response.statusCode === 200) counter++
-  })
+        if (response.statusCode === 200) counter++
+      })
 }
 
 setInterval(vote, 3000)
+
+const requestListener = function vote(req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+}
 
 const server = http.createServer(requestListener);
 // this should be the last line
