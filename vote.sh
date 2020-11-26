8707be58443686206190251633af91f4636c18b4
Veller <vellerbauer@gmail.com>
2020-11-26 00:30:11 -0300

    making it better

--- vote.sh
+++ vote.sh
@@ -2,7 +2,7 @@ VOTES=0;
 while :; do
   curl 'https://voting-vote-producer.r7.com/vote' \
   -H 'authority: voting-vote-producer.r7.com' \
-  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36' \
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36' \
   -H 'content-type: application/x-www-form-urlencoded' \
   -H 'accept: */*' \
   -H 'origin: https://afazenda.r7.com' \
@@ -11,9 +11,9 @@ while :; do
   -H 'sec-fetch-dest: empty' \
   -H 'referer: https://afazenda.r7.com/a-fazenda-12/votacao' \
   -H 'accept-language: en-US,en;q=0.9,pt;q=0.8' \
-  --data-raw 'voting_id=268&alternative_id=648' \
+  --data-raw 'voting_id=271&alternative_id=658' \
   --compressed
   sleep 2;
   let VOTES=VOTES+1;
-  echo Mateus vai ficar sim senhor. Votos: "$VOTES"
+  echo Mariano vai ficar sim senhor. Votos: "$VOTES"
 done
